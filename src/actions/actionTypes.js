b3ccad461f5b3a946169bfb11adff07ae917b08f
Hadeer <Hadeerashraf189@gmail.com>
2021-07-03 20:00:35 +0200

    add new file includes types of actions

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,14 @@
+// users
+export const GET_USERS="GET_USERS"
+
+// authuser
+export const SET_CURRENT_USERS="SET_CURRENT_USERS"
+
+
+// questions
+export const ADD_QUESTION="ADD_QUESTION"
+
+export const ANSWER_QUESTION="ANSWER_QUESTION"
+
+export const USERS_QUESTIONS="USERS_QUESTIONS"
+export const SHOW_QUESTION="SHOW_QUESTION"
