21e4db64f2e36d6a26013e6d82444be2447da467
Hadeer <Hadeerashraf189@gmail.com>
2021-07-03 19:56:12 +0200

    add app architecture

---
+++ startPlan.js
@@ -0,0 +1,32 @@
+//my app
+//app component the main ** exist **
+
+//sign in component:
+//form of select input including users names
+//action-->sign in forword him/her to home component
+//reducer -->add name and avatar 
+
+
+//nav component -->change its items depend the user login 
+//using navlink to components 
+//routing to check about user login or not
+//take signup reducer of show user's info 
+
+//home comp -->users' questions unanswered to me or answered:
+//2 lists including the questions with each user name and avatar  
+/*action to each question -->to view question deatils if it answered or not + 
+the answer if answered and the quetion if not*/
+//nested action in the unanswerd list of each question --> to submit answer
+//reducer-->add the new answerd question 
+
+//new question conponent:
+//new question form include 2 input + submit btn
+// action -->adding new question by the user
+//reducer-->add the new  question 
+
+//leader board comp -->the rank of effective users depending on the score(info)
+//reducer -->change the rank depends on the change happens in home 
+
+//logout --> link to signup page exists just when the user login forword to signin comp
+
+//user's name and avatar in the nav just when the user login (info)
