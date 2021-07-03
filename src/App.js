fbfc7274fb66dd5edf1b398d76b3f2869742b44d
Hadeer <Hadeerashraf189@gmail.com>
2021-07-03 20:26:19 +0200

    edit the whole file to fit what was added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import NewQue from "./components/NewQue";
+import LeaderBoard from "./components/LeaderBoard";
+import Home from "./components/Questions";
+import QuestionDetails from "./components/QuestionDetails";
+import Nav from "../src/components/NavBar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  handleSecurity = () => {
+    if (Object.keys(this.props.authUser).length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {Object.keys(this.props.authUser).length === 0 ? (
+          <Switch>
+            <Route path="/signin" component={SignIn}></Route>
+          </Switch>
+        ) : (
+          <>
+            <Nav></Nav>
+            <Switch>
+              <Route path="/home" component={Home}></Route>
+              <Route path="/add" component={NewQue}></Route>
+              <Route path="/leaderboard" component={LeaderBoard}></Route>
+              <Route path="/quedetails/:id" component={QuestionDetails}></Route>
+            </Switch>
+          </>
+        )}
+        <Redirect exact from="/" to="/signin" />
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  authUser: state.authUser,
+});
+
+export default connect(mapStateToProps)(App);
