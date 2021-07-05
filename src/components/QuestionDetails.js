c0b800c2bcce2969ddc8d188c0dcef632f12d2eb
Hadeer <Hadeerashraf189@gmail.com>
2021-07-05 18:19:11 +0200

    edit in show vote info and it's data

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -2,14 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 import { answerQuestion } from "../handlers/questionsApiData";
 import Swal from "sweetalert2";
+import QuestionVoteCard from "./QuestionVoteCard";
+import { Link } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
 class QuestionDetails extends React.Component {
   state = {
     choice: "",
+    choicetext: "",
   };
 
   handleAnswer = (e) => {
     this.setState({ choice: e.target.value });
+    this.setState({ choicetext: e.target.name });
   };
 
   handleSubmit = (user, qid) => {
@@ -28,10 +33,9 @@ class QuestionDetails extends React.Component {
       Swal.fire({
         icon: "success",
         title: "Yes...",
-        titleText: `your choose ${answer}`,
-        text: "your answer has been submitted",
+        titleText: "your answer has been submitted",
       });
-      this.props.history.replace("/home");
+      <QuestionVoteCard></QuestionVoteCard>;
     }
   };
 
@@ -44,126 +48,140 @@ class QuestionDetails extends React.Component {
   };
 
   render() {
-    const { questionDetails, authUser, users } = this.props;
+    const { questionDetails, authUser, users, questions } = this.props;
 
     return (
       <React.Fragment>
         <div className="container">
-          <div className="row row-cols-5">
-            <div className="col">
-              <img src={users[questionDetails.author].avatarURL} alt="Logo" />
-            </div>
-            <div className="col">
-              <h3 className="title">{questionDetails.author} asks:</h3>
-            </div>
-            <div className="col-7">
-              <ul className="list-group">
-                <li className="list-group-item d-flex justify-content-between ">
-                  <h5>Would You Rather...</h5>
-                </li>
-                <li className="list-group-item d-flex justify-content-between ">
-                  {questionDetails.optionOne.votes.includes(authUser.id) ? (
-                    <input
-                      checked
-                      disabled
-                      value="optionOne"
-                      className="btn-check"
-                      type="radio"
-                      id="choice1"
-                      name="choice"
-                      aria-label="Radio button for following text input"
-                    />
-                  ) : (
-                    <input
-                      onClick={(e) => this.handleAnswer(e)}
-                      value="optionOne"
-                      className="btn-check"
-                      type="radio"
-                      id="choice1"
-                      name="choice"
-                      aria-label="Radio button for following text input"
-                    />
-                  )}
-                  <label className="btn btn-outline-info" htmlFor="choice1">
-                    {questionDetails.optionOne.text}
-                  </label>
-                  <span className="badge bg-primary rounded-pill">
-                    Votes: {questionDetails.optionOne.votes.length}
-                  </span>
-                  <span className="badge bg-primary rounded-pill">
-                    Percentage:{" "}
-                    {Math.round(
-                      (questionDetails.optionOne.votes.length /
-                        Object.keys(users).length) *
-                        100
+          {!Object.keys(questions).includes(questionDetails.id) && authUser ? (
+            <ErrorPage></ErrorPage>
+          ) : (
+            <div className="row row-cols-5">
+              <div className="col">
+                <img src={users[questionDetails.author].avatarURL} alt="Logo" />
+              </div>
+              <div className="col">
+                <h3 className="title">{questionDetails.author} asks:</h3>
+              </div>
+              <div className="col-7">
+                <ul className="list-group">
+                  <li className="list-group-item d-flex justify-content-between ">
+                    <h5>Would You Rather...</h5>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between ">
+                    {questionDetails.optionOne.votes.includes(authUser.id) ? (
+                      <input
+                        checked
+                        disabled
+                        value="optionOne"
+                        className="btn-check"
+                        type="radio"
+                        id="choice1"
+                        name="choice"
+                        aria-label="Radio button for following text input"
+                      />
+                    ) : (
+                      <input
+                        onClick={(e) => this.handleAnswer(e)}
+                        value="optionOne"
+                        className="btn-check"
+                        type="radio"
+                        id="choice1"
+                        name={questionDetails.optionOne.text}
+                        aria-label="Radio button for following text input"
+                      />
                     )}
-                    %
-                  </span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between ">
-                  {questionDetails.optionTwo.votes.includes(authUser.id) ? (
-                    <input
-                      checked
-                      disabled
-                      value="optionTwo"
-                      className="btn-check"
-                      type="radio"
-                      id="choice2"
-                      name="choice"
-                      aria-label="Radio button for following text input"
-                    />
-                  ) : (
-                    <input
-                      onClick={(e) => this.handleAnswer(e)}
-                      value="optionTwo"
-                      className="btn-check"
-                      type="radio"
-                      id="choice2"
-                      name="choice"
-                      aria-label="Radio button for following text input"
-                    />
-                  )}
-                  <label className="btn btn-outline-info" htmlFor="choice2">
-                    {questionDetails.optionTwo.text}
-                  </label>
-                  <span className="badge bg-primary rounded-pill">
-                    Votes: {questionDetails.optionTwo.votes.length}
-                  </span>
-                  <span className="badge bg-primary rounded-pill">
-                    Percentage:{" "}
-                    {Math.round(
-                      (questionDetails.optionTwo.votes.length /
-                        Object.keys(users).length) *
-                        100
+                    <label className="btn btn-outline-info" htmlFor="choice1">
+                      {questionDetails.optionOne.text}
+                    </label>
+                    <span className="badge bg-primary rounded-pill">
+                      Votes: {questionDetails.optionOne.votes.length}
+                    </span>
+                    <span className="badge bg-primary rounded-pill">
+                      Percentage:{" "}
+                      {Math.round(
+                        (questionDetails.optionOne.votes.length /
+                          Object.keys(users).length) *
+                          100
+                      )}
+                      %
+                    </span>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between ">
+                    {questionDetails.optionTwo.votes.includes(authUser.id) ? (
+                      <input
+                        checked
+                        disabled
+                        value="optionTwo"
+                        className="btn-check"
+                        type="radio"
+                        id="choice2"
+                        name="choice"
+                        aria-label="Radio button for following text input"
+                      />
+                    ) : (
+                      <input
+                        onClick={(e) => this.handleAnswer(e)}
+                        value="optionTwo"
+                        className="btn-check"
+                        type="radio"
+                        id="choice2"
+                        name={questionDetails.optionTwo.text}
+                        aria-label="Radio button for following text input"
+                      />
                     )}
-                    %
-                  </span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between">
-                  {questionDetails.optionOne.votes.includes(authUser.id) ||
-                  questionDetails.optionTwo.votes.includes(authUser.id) ? (
-                    <button
-                      onClick={this.handleAnsweredQue}
-                      type="submit"
-                      className="btn btn-outline-info"
-                    >
-                      Submit
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() =>
-                        this.handleSubmit(authUser, questionDetails)
-                      }
-                      type="submit"
-                      className="btn btn-outline-info"
-                    >
-                      Submit
-                    </button>
-                  )}
-                </li>
-              </ul>
+                    <label className="btn btn-outline-info" htmlFor="choice2">
+                      {questionDetails.optionTwo.text}
+                    </label>
+                    <span className="badge bg-primary rounded-pill">
+                      Votes: {questionDetails.optionTwo.votes.length}
+                    </span>
+                    <span className="badge bg-primary rounded-pill">
+                      Percentage:{" "}
+                      {Math.round(
+                        (questionDetails.optionTwo.votes.length /
+                          Object.keys(users).length) *
+                          100
+                      )}
+                      %
+                    </span>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between">
+                    {questionDetails.optionOne.votes.includes(authUser.id) ||
+                    questionDetails.optionTwo.votes.includes(authUser.id) ? (
+                      <button
+                        onClick={this.handleAnsweredQue}
+                        type="submit"
+                        className="btn btn-outline-info"
+                      >
+                        Submit
+                      </button>
+                    ) : (
+                      <Link
+                        to={{
+                          pathname: `/queVote`,
+                          state: {
+                            choicetext: this.state.choicetext,
+                            choice: this.state.choice,
+                          },
+                        }}
+                      >
+                        <button
+                          onClick={() =>
+                            this.handleSubmit(authUser, questionDetails)
+                          }
+                          type="submit"
+                          className="btn btn-outline-info"
+                        >
+                          Submit
+                        </button>
+                      </Link>
+                    )}
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </React.Fragment>
     );
@@ -174,6 +192,7 @@ const mapStateToProps = (state) => ({
   questionDetails: state.questionDetails,
   authUser: state.authUser,
   users: state.users,
+  questions: state.questions,
   answerInfo: state.answerInfo,
 });
 
