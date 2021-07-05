42bbfe6d7fff00dabb06ca46c42548b0cff18464
Hadeer <Hadeerashraf189@gmail.com>
2021-07-05 18:21:01 +0200

    add new component to render undefined poll

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ErrorPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <h1>Poll is not exist</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ErrorPage;
