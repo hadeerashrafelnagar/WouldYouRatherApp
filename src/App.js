1a55eacd0fdf573dd3c17e38f29b9fee5cb9fc64
Hadeer <Hadeerashraf189@gmail.com>
2021-07-05 18:23:57 +0200

    edit routing part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import NewQue from "./components/NewQue";
 import LeaderBoard from "./components/LeaderBoard";
 import Home from "./components/Questions";
+import QuestionVoteCard from "./components/QuestionVoteCard";
 import QuestionDetails from "./components/QuestionDetails";
+import ErrorPage from "./components/PrivateRoute";
 import Nav from "../src/components/NavBar";
 import "./App.css";
 
@@ -17,26 +19,26 @@ class App extends React.Component {
       return true;
     }
   };
-
   render() {
     return (
       <React.Fragment>
         {Object.keys(this.props.authUser).length === 0 ? (
           <Switch>
-            <Route path="/signin" component={SignIn}></Route>
+            <Route path="/" component={SignIn}></Route>
           </Switch>
         ) : (
           <>
             <Nav></Nav>
             <Switch>
+              <Route path="/questions/error" component={ErrorPage}></Route>
               <Route path="/home" component={Home}></Route>
               <Route path="/add" component={NewQue}></Route>
               <Route path="/leaderboard" component={LeaderBoard}></Route>
-              <Route path="/quedetails/:id" component={QuestionDetails}></Route>
+              <Route path="/questions/:id" component={QuestionDetails}></Route>
+              <Route path="/queVote" component={QuestionVoteCard}></Route>
             </Switch>
           </>
         )}
-        <Redirect exact from="/" to="/signin" />
       </React.Fragment>
     );
   }
